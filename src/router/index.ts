fcf893d06da687018edeee9315f362ec72858cc2
amtoaer <amtoaer@gmail.com>
2021-02-21 17:54:46 +0800

    feat: init the project

---
+++ src/router/index.ts
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: ()=>import('@/views/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to,from,next)=>{
+  NProgress.start()
+  next()
+})
+
+router.afterEach(()=>{
+  NProgress.done()
+})
+
+export default router
