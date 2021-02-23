8c00e1e2280e94f807cd83ac6c471677e005276c
amtoaer <amtoaer@gmail.com>
2021-02-23 17:21:22 +0800

    feat(app): complete the layout

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,78 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Index</router-link> |
-      <router-link to="/Login">Login</router-link>
-    </div> -->
-    <router-view />
-  </div>
+  <a-layout id="components-layout-custom-trigger">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="inline"
+        :default-selected-keys="['1']"
+        :default-open-keys="['sub1']"
+      >
+        <a-sub-menu v-for="submenu in sidebar" :key="submenu.key">
+          <span slot="title"
+            ><a-icon :type="submenu.icon" />{{ submenu.title }}</span
+          >
+          <a-menu-item v-for="menu in submenu.menus" :key="menu.key">
+            <router-link :to="menu.path">
+              <a-icon :type="menu.icon" />
+              <span>{{ menu.text }}</span>
+            </router-link>
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
+      >
+        <router-view />
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { Layout, Menu, Icon } from "ant-design-vue";
+
+Vue.use(Layout).use(Menu).use(Icon);
+
 export default Vue.extend({
+  data: function () {
+    return {
+      collapsed: false,
+      sidebar: [
+        {
+          title: "用户功能",
+          key: "sub1",
+          icon: "user",
+          menus: [
+            { key: "1", path: "/login", icon: "login", text: "登录" },
+            { key: "2", path: "/register", icon: "user-add", text: "注册" },
+          ],
+        },
+      ],
+    };
+  },
+  watch: {},
   mounted: function () {
     if (
       localStorage.getItem("token") === null ||
       localStorage.getItem("user") === null
     ) {
-      this.$msg.error("您还未登录，正在跳转至登陆界面...");
+      this.$msg.error("您还未登录，正在跳转至登录界面...");
       this.$router.replace({
         path: "/login",
       });
@@ -29,3 +85,28 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style>
+#components-layout-custom-trigger {
+  width: 100%;
+  height: 100%;
+}
+
+#components-layout-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
